e641e55b9c75a6a1230757c536d68161d600a813
Paul Nykiel <paul.nykiel@gmail.com>
2019-05-18 21:37:43 +0200

    Better naming

--- rc_lib.h
+++ rc_lib.h
@@ -36,7 +36,7 @@ void rc_lib_init_rx(rc_lib_package_t* package);
 void rc_lib_init_tx(rc_lib_package_t* package, uint16_t resolution, uint8_t channel_count);
 uint8_t rc_lib_encode(rc_lib_package_t* package);
 uint8_t rc_lib_decode(rc_lib_package_t* package, uint8_t data);
-uint8_t rc_lib_calculate_checksum(rc_lib_package_t* checksum);
+uint8_t rc_lib_calculate_checksum(rc_lib_package_t* package);
 uint8_t is_discover_message(rc_lib_package_t* package);
 uint8_t is_discover_response(rc_lib_package_t* package);
 void set_discover_message(rc_lib_package_t* package);
